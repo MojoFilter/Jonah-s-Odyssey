2065e87241af93deef920298fe546b96c2f4a8b1
Josh Weeks <weeks.josh@gmail.com>
2020-08-25 01:38:25 -0400

    initial Jonah rig

---
+++ Assets/InputActions/JonahControls.cs
@@ -0,0 +1,245 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/InputActions/JonahControls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @JonahControls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @JonahControls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""JonahControls"",
+    ""maps"": [
+        {
+            ""name"": ""Normal"",
+            ""id"": ""700d8b34-8454-42a5-8b65-0c169e0327be"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""a5209aa0-9e0a-4eec-a63c-423840c17562"",
+                    ""expectedControlType"": ""Axis"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""782438ed-f62d-46b4-aad4-e9cec9af66ad"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Crouch"",
+                    ""type"": ""Button"",
+                    ""id"": ""c48d0b8c-b2bd-4da3-99ec-d26c2c86599e"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""603bc4b8-04a2-443d-b7dc-fa95271adf6c"",
+                    ""path"": ""<Gamepad>/leftStick/x"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""1D Axis"",
+                    ""id"": ""adca1498-0fcf-41f9-895b-8ca06716112d"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""c36a6747-5a3a-457a-a5c5-c1e37b96a8cf"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""1e258b0e-d40c-47d4-b252-203b67f1b21f"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""b47b97da-f5c2-473c-bd93-d146efe9b7e0"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""51796fb0-f279-488f-8bb7-d192bc5f4d11"",
+                    ""path"": ""<Gamepad>/buttonSouth"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""68b90096-fa9f-4212-9619-124802dce53a"",
+                    ""path"": ""<Gamepad>/buttonEast"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Crouch"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""dd482029-9f09-4241-8dbc-3e83cab0154a"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Crouch"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Normal
+        m_Normal = asset.FindActionMap("Normal", throwIfNotFound: true);
+        m_Normal_Move = m_Normal.FindAction("Move", throwIfNotFound: true);
+        m_Normal_Jump = m_Normal.FindAction("Jump", throwIfNotFound: true);
+        m_Normal_Crouch = m_Normal.FindAction("Crouch", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Normal
+    private readonly InputActionMap m_Normal;
+    private INormalActions m_NormalActionsCallbackInterface;
+    private readonly InputAction m_Normal_Move;
+    private readonly InputAction m_Normal_Jump;
+    private readonly InputAction m_Normal_Crouch;
+    public struct NormalActions
+    {
+        private @JonahControls m_Wrapper;
+        public NormalActions(@JonahControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_Normal_Move;
+        public InputAction @Jump => m_Wrapper.m_Normal_Jump;
+        public InputAction @Crouch => m_Wrapper.m_Normal_Crouch;
+        public InputActionMap Get() { return m_Wrapper.m_Normal; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(NormalActions set) { return set.Get(); }
+        public void SetCallbacks(INormalActions instance)
+        {
+            if (m_Wrapper.m_NormalActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_NormalActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_NormalActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_NormalActionsCallbackInterface.OnMove;
+                @Jump.started -= m_Wrapper.m_NormalActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_NormalActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_NormalActionsCallbackInterface.OnJump;
+                @Crouch.started -= m_Wrapper.m_NormalActionsCallbackInterface.OnCrouch;
+                @Crouch.performed -= m_Wrapper.m_NormalActionsCallbackInterface.OnCrouch;
+                @Crouch.canceled -= m_Wrapper.m_NormalActionsCallbackInterface.OnCrouch;
+            }
+            m_Wrapper.m_NormalActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+                @Crouch.started += instance.OnCrouch;
+                @Crouch.performed += instance.OnCrouch;
+                @Crouch.canceled += instance.OnCrouch;
+            }
+        }
+    }
+    public NormalActions @Normal => new NormalActions(this);
+    public interface INormalActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+        void OnCrouch(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Scripts/JonahController.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class JonahController : MonoBehaviour
+{
+    [SerializeField]
+    private float _walkSpeed = 5f;
+
+    [SerializeField] 
+    private float _jumpForce = 100f;
+
+    [SerializeField]
+    private Animator _animator;
+
+    [SerializeField]
+    private Rigidbody2D _rigidbody;
+
+    [SerializeField]
+    private LayerMask _groundLayers;
+
+    [SerializeField]
+    private Transform _groundCheck;
+
+    private const float _groundingRadius = .2f;
+
+    private bool _isGrounded = false;
+    private JonahControls _controls;
+
+    private void Awake()
+    {
+        _controls = new JonahControls();
+
+        _controls.Normal.Crouch.started += _ => this.Crouch(true);
+        _controls.Normal.Crouch.canceled += _ => this.Crouch(false);
+
+        _controls.Normal.Jump.performed += _ => this.Jump();
+    }
+
+
+    private void OnEnable()
+    {
+        _controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _controls.Disable();
+    }
+
+    private void FixedUpdate()
+    {
+        this.Move(_controls.Normal.Move.ReadValue<float>());
+        this.CheckForGrounding();
+    }
+
+    private void Move(float velocity)
+    {
+        var magnitude = Mathf.Abs(velocity);
+        if (!_animator.GetBool("IsCrouching"))
+        {
+            _rigidbody.velocity = new Vector2(velocity * _walkSpeed, _rigidbody.velocity.y);
+            _animator.SetFloat("Speed", magnitude);
+            if (magnitude > 0f)
+            {
+                var xScale = velocity < 0f ? -1f : 1f;
+                this.transform.localScale = new Vector3(xScale, 1f, 1f);
+            }
+        }
+    }
+
+    private void Crouch(bool crouch)
+    {
+        _animator.SetBool("IsCrouching", crouch);
+    }
+
+    private void Jump()
+    {
+        if (_isGrounded)
+        {
+            _rigidbody.AddForce(new Vector2(0f, _jumpForce));
+        }
+    }
+
+
+    private void CheckForGrounding()
+    {
+        var collisons = Physics2D.OverlapCircleAll(_groundCheck.position, _groundingRadius, _groundLayers);
+        if (collisons.Any(c => c.gameObject != this.gameObject))
+        {
+            if (!_isGrounded)
+            {
+                this.Ground(true);
+            }
+        }
+        else
+        {
+            this.Ground(false);
+        }
+    }
+
+    private void Ground(bool grounded)
+    {
+        _animator.SetBool("IsJumping", !grounded);
+        _isGrounded = grounded;
+    }
+}
